54d4ada5a1f4ca0b77fd746eeb7aa4ffbe02929b
toumtam2100 <toumtampde160576@fpt.edu.vn>
2023-09-12 21:37:27 +0700

    done

---
+++ gm-backend/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://localhost:27017/conFusion",
+  facebook: {
+    clientId: "641242911006815",
+    clientSecret: "75ed07e681aa76cde433e570d10c248a",
+  },
+};
